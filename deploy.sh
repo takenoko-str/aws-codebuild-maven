7094354175bc6cb17d9e41ea56f37f33f8e411b5
takenoko-str <taku.hyogo.kei@gmail.com>
2018-11-26 13:57:52 +0900

    再起動コマンド追加

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/opt/glassfish4/bin/asadmin redeploy \
+sudo -u glassfish /opt/glassfish4/bin/asadmin redeploy \
     --name=aws-codebuild-maven \
     /var/www/html/target/aws-codebuild-maven.war
+
+sudo -u glassfish /opt/glassfish4/bin/asadmin restart-domain
 
